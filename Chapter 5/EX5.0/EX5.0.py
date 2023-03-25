fc189cd2e7adce1f79eb0a284f89549fddfdd7cb
JohnRadtke <122950114+JohnRadtke@users.noreply.github.com>
2023-03-25 13:50:14 -0400

    Add files via upload

---
+++ Chapter 5/EX5.0/EX5.0.py
@@ -0,0 +1,22 @@
+
+count = 0
+total = 0
+
+while True:
+    user_input = input("Enter a number: ")
+    if user_input == 'done':
+        break
+    
+    try:
+        number = float(user_input)
+        count += 1
+        total += number
+    except:
+        print("Invalid input")
+        continue
+
+if count > 0:
+    average = total / count
+    print(total, count, average)
+else:
+    print("No valid numbers entered.")
